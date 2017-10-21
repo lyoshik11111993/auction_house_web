44ee930cc4895b19bbaa260a5a9599ea9962a533
lyoshik <lyoshikrul@gmail.com>
2017-10-21 09:16:59 +0300

    added model attribute

--- src/main/java/com/example/web/controller/InsertController.java
+++ src/main/java/com/example/web/controller/InsertController.java
@@ -1,14 +1,17 @@
 package com.example.web.controller;
 
 import com.example.web.config.AppPaths;
+import com.example.web.dto.SellerDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class InsertController {
 
     @RequestMapping(AppPaths.INSERT)
-    public String get() {
+    public String get(Model model) {
+        model.addAttribute("seller", new SellerDTO());
         return "insert";
     }
 }
